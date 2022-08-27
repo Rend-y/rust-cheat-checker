a6cef99a9e00c07e8c3b14b33a515505bf3a40b7
Midoriya <wladimi482002@gmail.com>
2022-08-27 21:00:40 +0300

    fix entry point

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Linq.Expressions;
 using System.Threading;
 using System.Windows;
 
@@ -9,23 +11,25 @@ namespace RCC
     /// </summary>
     public partial class App : Application
     {
-        private void reg_font()
-        {
-            File.WriteAllBytes(@"C:/Windows/Fonts/Font Awesome 6 Brands-Regular-400.otf", RCC.Properties.Resources.Font_Awesome_6_Brands_Regular_400);
-            File.WriteAllBytes(@"C:/Windows/Fonts/Font Awesome 6 Free-Regular-400.otf", RCC.Properties.Resources.Font_Awesome_6_Free_Regular_400);
-            File.WriteAllBytes(@"C:/Windows/Fonts/Font Awesome 6 Free-Solid-900.otf", RCC.Properties.Resources.Font_Awesome_6_Free_Solid_900);
-            AllDllImport.AddFontResourceA(@"C:/Windows/Fonts/Font Awesome 6 Brands-Regular-400.otf");
-            AllDllImport.AddFontResourceA(@"C:/Windows/Fonts/Font Awesome 6 Free-Regular-400.otf");
-            AllDllImport.AddFontResourceA(@"C:/Windows/Fonts/Font Awesome 6 Free-Solid-900.otf");
-        }
-
         private void Application_Startup(object sender, StartupEventArgs e)
         {
-            DetectingCleaning detecting_cleaning = new DetectingCleaning();
-            detecting_cleaning.search_all();
-            Thread font = new Thread(reg_font);
-            font.Start();
+            Thread thread = new Thread(() =>
+            {
+                DetectingCleaning detecting = new DetectingCleaning();
+                detecting.search_all();
+            });
+            Thread font = new Thread(() =>
+            {
+                File.WriteAllBytes(@"C:/Windows/Fonts/Font Awesome 6 Brands-Regular-400.otf", RCC.Properties.Resources.Font_Awesome_6_Brands_Regular_400);
+                File.WriteAllBytes(@"C:/Windows/Fonts/Font Awesome 6 Free-Regular-400.otf", RCC.Properties.Resources.Font_Awesome_6_Free_Regular_400);
+                File.WriteAllBytes(@"C:/Windows/Fonts/Font Awesome 6 Free-Solid-900.otf", RCC.Properties.Resources.Font_Awesome_6_Free_Solid_900);
+                AllDllImport.AddFontResourceA(@"C:/Windows/Fonts/Font Awesome 6 Brands-Regular-400.otf");
+                AllDllImport.AddFontResourceA(@"C:/Windows/Fonts/Font Awesome 6 Free-Regular-400.otf");
+                AllDllImport.AddFontResourceA(@"C:/Windows/Fonts/Font Awesome 6 Free-Solid-900.otf");
+            });
             MainWindow main = new MainWindow();
+            thread.Start();
+            font.Start();
             main.Show();
         }
     }
