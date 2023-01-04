e440a1f7bdbc7516aadd8b76d63e0349d6ce611c
Midoriya <wladimi482002@gmail.com>
2023-01-04 11:44:14 +0300

    add dangerous application

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -25,13 +25,13 @@ namespace RCC
         private readonly LastActivityPage lastActivityPage = new LastActivityPage();
         private readonly UsbDevicePage usbDevicesPage = new UsbDevicePage();
         private readonly SearchFilePage searchFilePage = new SearchFilePage();
+        private readonly OtherPage otherPage = new OtherPage();
+        
         public main_window()
         {
             InitializeComponent();
 
             WindowsPageManager(new GreetingPage());
-
-            DangerousApps.start_scan().ForEach(item => list_all_dangerous_apps.Items.Add(item));
         }
         private void WindowsPageManager(Page newPage) => PagesFrame.Content = newPage; 
         private void button_show_account_info_page_MouseDown(object sender, MouseButtonEventArgs e) => WindowsPageManager(steamDataPage);
@@ -39,7 +39,7 @@ namespace RCC
         private void button_show_last_activity_page_MouseDown(object sender, MouseButtonEventArgs e) => WindowsPageManager(lastActivityPage);
         private void button_show_mouse_check_MouseDown(object sender, MouseButtonEventArgs e) => WindowsPageManager(mouseLoggerPage);
         private void Button_show_search_file_OnMouseDown(object sender, MouseButtonEventArgs e) => WindowsPageManager(searchFilePage);
-        private void button_show_other_page_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) => WindowsPageManager(new SteamDataPage());
+        private void button_show_other_page_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) => WindowsPageManager(otherPage);
         private void grid_custom_title_bar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) => DragMove();
         private void label_close_application_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) => Environment.Exit(Environment.ExitCode);
         private void label_turn_off_application_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) => this.WindowState = WindowState.Minimized;
---
+++ Pages/OtherPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+
+namespace RCC.Pages
+{
+    public partial class OtherPage : Page
+    {
+        public OtherPage()
+        {
+            InitializeComponent();
+            
+            DangerousApps.start_scan().ForEach(item => ListAllDangerousApps.Items.Add(item));
+        }
+    }
+}
